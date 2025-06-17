2f0aa3c7a26c63a78b5ced57095f8b2767a2164e
Bruno Oliveira <brunogabriel2608@gmail.com>
2025-06-16 21:48:00 -0300

    colocando explicacao do codigo de exemplo

--- example.py
+++ example.py
@@ -1,5 +1,9 @@
+#Rode somente o container da ollama
+#Use esse comando para rodar o seu modelo de preferencia 'docker exec -it ollama ollama run model_namev1'
+#Faça o comando pip install ollama e rode o codigo abaixo
+
+
 import ollama
 
-# Use the generate function for a one-off prompt
-result = ollama.generate(model='deepseek-r1:1.5b', prompt='Por que o ceu é azul?')
+result = ollama.generate(model='model_namev1', prompt='Por que o ceu é azul?')
 print(result['response'])
